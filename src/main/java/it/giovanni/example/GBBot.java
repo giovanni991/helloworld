c9ad3586f6bfbec872d9a3fc4869440aa7438671
giovanni <giovanni@192.168.1.91>
2017-05-23 20:33:47 +0200

    test

---
+++ src/main/java/it/giovanni/example/GBBot.java
@@ -0,0 +1,45 @@
+package it.giovanni.example;
+
+
+import org.apache.commons.lang3.StringUtils;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class GBBot extends TelegramLongPollingBot {
+    @Override
+    public void onUpdateReceived(Update update) {
+    	// We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            if(StringUtils.containsIgnoreCase(message_text, "tilapia"))
+        		message_text = "Di mare";
+            
+            SendMessage message = new SendMessage() // Create a message object object
+                    .setChatId(chat_id)
+                    .setText(message_text);
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        // Return bot username
+        // If bot username is @MyAmazingBot, it must return 'MyAmazingBot'
+        return "Malgarbot";
+    }
+
+    @Override
+    public String getBotToken() {
+        // Return bot token from BotFather
+        return "365496129:AAG96gyhylXbJNZiIF4PWhsK-i-Xuoe49lU";
+    }
+}
---
+++ src/main/java/it/giovanni/example/Main.java
@@ -0,0 +1,23 @@
+package it.giovanni.example;
+
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Main {
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new GBBot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
