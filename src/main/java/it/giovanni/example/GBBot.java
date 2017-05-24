13a056fb4cabaf6145e0d417b50f4b1949b44c49
giovanni <giovanni@192.168.1.91>
2017-05-24 22:01:17 +0200

    aggiunti canali per poci

--- src/main/java/it/giovanni/example/GBBot.java
+++ src/main/java/it/giovanni/example/GBBot.java
@@ -13,21 +13,69 @@ public class GBBot extends TelegramLongPollingBot {
     	// We check if the update has a message and the message has text
         if (update.hasMessage() && update.getMessage().hasText()) {
             // Set variables
-            String message_text = update.getMessage().getText();
-            long chat_id = update.getMessage().getChatId();
-
-            if(StringUtils.containsIgnoreCase(message_text, "tilapia"))
-        		message_text = "Di mare";
+            String user_username = update.getMessage().getChat().getUserName();
             
+            if("giovanni9".equals(user_username)){
+            	giovanniChannel(update);
+            }else if("AndreaBF".equals(user_username)){
+            	giangiChannel(update);
+            }
+           
+        }
+    }
+    
+    public void giovanniChannel(Update update){
+    	long chat_id = update.getMessage().getChatId();
+    	String message_text = update.getMessage().getText();
+    	if(StringUtils.containsIgnoreCase(message_text, "debug")){
             SendMessage message = new SendMessage() // Create a message object object
-                    .setChatId(chat_id)
-                    .setText(message_text);
+                .setChatId(chat_id)
+                .setText(update.toString());
             try {
                 sendMessage(message); // Sending our message object to user
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
+    	
+    	if(StringUtils.containsIgnoreCase(message_text, "tilapia"))
+    		message_text = "Di mare";
+    	
+    	 SendMessage message = new SendMessage() // Create a message object object
+                 .setChatId(chat_id)
+                 .setText(message_text);
+         try {
+             sendMessage(message); // Sending our message object to user
+         } catch (TelegramApiException e) {
+             e.printStackTrace();
+         }
+    	
+    }
+    
+    public void giangiChannel(Update update){
+    	
+    	long chat_id = update.getMessage().getChatId();
+    	String message_text = update.getMessage().getText();
+    	if(StringUtils.containsIgnoreCase(message_text, "debug")){
+            SendMessage message = new SendMessage() // Create a message object object
+                .setChatId(chat_id)
+                .setText(update.toString());
+            try {
+                sendMessage(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    	
+    	 SendMessage message = new SendMessage() // Create a message object object
+                 .setChatId(chat_id)
+                 .setText(message_text);
+         try {
+             sendMessage(message); // Sending our message object to user
+         } catch (TelegramApiException e) {
+             e.printStackTrace();
+         }
+    	
     }
 
     @Override
